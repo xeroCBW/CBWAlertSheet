f533edede995c55479f13a671ac824033c4be78d
chenBW <861754186@qq.com>
2016-11-29 10:54:19 +0800

    使用 autosizing 实现全屏适配;不再使用通知

--- CBWAlertSheet/CBWAlertSheet.m
+++ CBWAlertSheet/CBWAlertSheet.m
@@ -19,6 +19,9 @@ static float const dismisDelay = 0.0f;
 static float const buttonFont = 17.0;
 static float const buttonHeight  = 50.0;
 static float const gap = 10;//取消按钮与上面的 gap
+static int const cancelButtonTag = 999;
+static int const cancelGapViewtag = 1000;
+static int const sheetButtonMarginViewTag = 1999;
 
 #define screenW [UIScreen mainScreen].bounds.size.width
 #define screenH [UIScreen mainScreen].bounds.size.height
@@ -69,6 +72,20 @@ static float const gap = 10;//取消按钮与上面的 gap
 /** items 数组*/
 @property (nonatomic ,strong) NSMutableArray *items;
 
+/** 标题的 label*/
+@property (nonatomic, strong)CBWAlertSheetLabel *titleLabel;
+
+/** message的 label*/
+@property (nonatomic, strong)CBWAlertSheetLabel *messageLabel;
+
+@property (nonatomic, strong)UIView *marginView;
+
+@property (nonatomic, assign) CGSize titleSize;
+
+@property (nonatomic, assign) CGSize messageSize;
+
+@property (nonatomic, assign)CGRect cancelButtonFrame;
+
 @end
 
 @implementation CBWAlertSheet
@@ -82,16 +99,10 @@ static float const gap = 10;//取消按钮与上面的 gap
     if (self) {
         
         [self initBase];
-        [self addNotification];
-     
     }
     return self;
 }
 
--(void)dealloc{
-    
-    [[NSNotificationCenter defaultCenter] removeObserver:self];
-}
 
 - (instancetype)initWithTitle:(NSString *)title andMessage:(NSString *)message{
     self = [super init];
@@ -106,21 +117,13 @@ static float const gap = 10;//取消按钮与上面的 gap
 }
 
 - (void)initBase{
-    
-    self.frame = [UIScreen mainScreen].bounds;
+
+    self.autoresizingMask = UIViewAutoresizingFlexibleWidth;//设置尺寸跟随屏幕
+    self.autoresizingMask = UIViewAutoresizingFlexibleHeight;//设置尺寸跟随屏幕
     self.backgroundColor = [UIColor colorWithWhite:0.0 alpha:0.6];
     self.alpha = 0.0;
 
 }
-
-#pragma mark - notificatioin
-
-- (void)addNotification{
-    
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(orientChange:)name:UIDeviceOrientationDidChangeNotification object:nil];
-    
-}
-
 
 #pragma mark - subView
 
@@ -139,7 +142,6 @@ static float const gap = 10;//取消按钮与上面的 gap
     if (self.contentView == nil) {
         
         [self setUpDefaultView];
-        
     }
     
     if (self.type == CBWAlertSheetTypeCancelButton)
@@ -152,104 +154,50 @@ static float const gap = 10;//取消按钮与上面的 gap
 
 - (void)setUpDefaultView{
     
+    [self setUpContentView];
     
-    CGFloat x = 0;
-    CGFloat y = screenH;
-    CGFloat w = screenW;
+    [self setUpTitleAndMessageView];
     
-    NSString *title = self.title;
-    NSString *message = self.message;
+}
+
+- (void)setUpContentView{
     
-    UIFont *font = [UIFont fontWithName:@"HelveticaNeue-Bold" size:titleFont];
-    CGSize titleSize = [title boundingRectWithSize:CGSizeMake( w - 2*lrMargin, 0) options:NSStringDrawingUsesLineFragmentOrigin attributes:@{NSFontAttributeName:font} context:nil].size;//用粗体计算,否者会出现横线,不知道为什么
-    UIFont *msgFont = [UIFont fontWithName:@"HelveticaNeue" size:messageFont];
-    CGSize messageSize = [message boundingRectWithSize:CGSizeMake( w - 2*lrMargin, 0) options:NSStringDrawingUsesLineFragmentOrigin attributes:@{NSFontAttributeName:msgFont} context:nil].size;
-    //h需要改变
-    float h = 0;
+    UIView *contentView = [[UIView alloc] init];
+    contentView.frame = [self calculateContentViewFrameWithRatate:NO];
     
-    if (![self isBlankString:title]||![self isBlankString:message]) {
-        h = h + 2 * normalMargin + margin;
-       
-    }
     
-    if (![self isBlankString:title]) {
-        h = h + titleSize.height;
-    }
-    
-    if (![self isBlankString:message]) {
-         h = h + messageSize.height;
-    }
-    
-    if (![self isBlankString:message]&&![self isBlankString:title]) {
-        h = h + titleLabelBottomMargin;
-    }
-    
-    h = h + self.items.count * buttonHeight + (self.items.count - 1) * margin;
-    if (self.type == CBWAlertSheetTypeCancelButton) {h = h + gap + buttonHeight;}
-    
-    UIView *contentView = [[UIView alloc] initWithFrame:CGRectMake(x, y, w, h)];
     contentView.backgroundColor = [UIColor whiteColor];
     [self addSubview:contentView];
     self.contentView = contentView;
     
+}
+
+- (void)setUpTitleAndMessageView{
+   
+    NSString *title = self.title;
+    NSString *message = self.message;
     
     if (![self isBlankString:title]) {
         
-        CBWAlertSheetLabel *titleLabel = [[CBWAlertSheetLabel alloc]init];
-        titleLabel.textAlignment = NSTextAlignmentCenter;
-        titleLabel.text = title;
-        titleLabel.font = [UIFont fontWithName:@"HelveticaNeue" size:titleFont];
-        titleLabel.textColor = self.titleTextColor?self.titleTextColor:textDefaultColor;
-        titleLabel.textAlignment = NSTextAlignmentCenter;
-        titleLabel.backgroundColor = [UIColor whiteColor];
-        titleLabel.numberOfLines = 0;
-        titleLabel.frame = CGRectMake(0, 0, w, titleSize.height);
-        titleLabel.autoresizingMask = UIViewAutoresizingFlexibleWidth;//设置宽度跟随屏幕
-        titleLabel.center = CGPointMake(w * 0.5, normalMargin + titleSize.height * 0.5);
-        [contentView addSubview:titleLabel];
+        [self.contentView addSubview:self.titleLabel];
     }
-   
+    
     if (![self isBlankString:message]) {
-        // 初始化label
-        CBWAlertSheetLabel *messageLabel = [[CBWAlertSheetLabel alloc]init];
-        messageLabel.textAlignment = NSTextAlignmentCenter;
-        messageLabel.textColor = self.messageTextColor?self.messageTextColor:textDefaultColor;
-        // label获取字符串
-        messageLabel.text = message;
-        messageLabel.backgroundColor = [UIColor whiteColor];
-        // label获取字体
-        messageLabel.font = [UIFont fontWithName:@"HelveticaNeue" size:messageFont];
-        // 设置无限换行
-        messageLabel.numberOfLines = 0;
-        float messageY = [self isBlankString:title] ?
-        normalMargin :
-        normalMargin + titleSize.height + titleLabelBottomMargin;
-        messageLabel.frame = CGRectMake( 0, messageY, w, messageSize.height);
-        messageLabel.autoresizingMask = UIViewAutoresizingFlexibleWidth;//设置宽度跟随屏幕
-        [contentView addSubview:messageLabel];
+       
+        [self.contentView addSubview:self.messageLabel];
     }
     
     if (![self isBlankString:title]||![self isBlankString:message]) {
-       
-      float delta = (self.type == CBWAlertSheetTypeCancelButton) ?
-      gap + buttonHeight:0;
-        
-        float x = 0;
-        float y = h - delta - margin - (self.items.count * buttonHeight + (self.items.count - 1) * margin);
-        float w = screenW;
-        float h = margin;
-        
-        UIView *margin = [[UIView alloc]initWithFrame:CGRectMake(x, y, w, h)];
-        margin.autoresizingMask = UIViewAutoresizingFlexibleWidth;
-        margin.backgroundColor = marginColor;
-        [contentView addSubview:margin];
+
+        [self.contentView addSubview:self.marginView];
     }
-   
-   }
+
+}
 
 - (void)setUpcancelButton{
     
     UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+    button.tag = cancelButtonTag;
     [button setTitle:@"取消" forState:UIControlStateNormal];
     UIColor *itemColor = self.cancleButtonTextColor?self.cancleButtonTextColor:cancelButtonTitleColor;
     [button setTitleColor:itemColor forState:UIControlStateNormal];
@@ -259,16 +207,13 @@ static float const gap = 10;//取消按钮与上面的 gap
     UIColor *cancleButtonNormalColor = self.cancleButtonColor ? self.cancleButtonColor:cancelButtonNormalColor;
     [button setBackgroundImage:[self imageWithColor:cancleButtonNormalColor] forState:UIControlStateNormal];
 
-    float btnX = 0;
-    float btnY = self.contentView.bounds.size.height - buttonHeight;
-    float btnW = screenW;
-    float btnH = buttonHeight;
-    [button setFrame:CGRectMake(btnX,btnY, btnW, btnH)];//开始预设宽度,后面屏幕旋转会UIViewAutoresizingFlexibleWidth进行跟随
+    [button setFrame:self.cancelButtonFrame];//开始预设宽度,后面屏幕旋转会UIViewAutoresizingFlexibleWidth进行跟随
     button.autoresizingMask = UIViewAutoresizingFlexibleWidth;//设置宽度屏幕跟随
     [button addTarget:self action:@selector(cancelButtonAction) forControlEvents:UIControlEventTouchUpInside];
     [self.contentView addSubview:button];
     
-    UIView *gapView = [[UIView alloc]initWithFrame:CGRectMake(0, btnY - gap, screenW, gap)];//开始预设宽度,后面屏幕旋转会UIViewAutoresizingFlexibleWidth进行跟随
+    UIView *gapView = [[UIView alloc]initWithFrame:CGRectMake(0, self.cancelButtonFrame.origin.y - gap, screenW, gap)];//开始预设宽度,后面屏幕旋转会UIViewAutoresizingFlexibleWidth进行跟随
+    gapView.tag = cancelGapViewtag;
     gapView.backgroundColor = gapColor;
     gapView.autoresizingMask = UIViewAutoresizingFlexibleWidth;//设置宽度屏幕跟随
     [self.contentView addSubview:gapView];
@@ -320,6 +265,8 @@ static float const gap = 10;//取消按钮与上面的 gap
             float sepratorViewH = margin;
             CGRect sepratorFrame = CGRectMake(sepratorViewX, sepratorViewY, sepratorViewW, sepratorViewH);
             UIView *sepratorView = [[UIView alloc]initWithFrame:sepratorFrame];
+            sepratorView.tag = sheetButtonMarginViewTag + i;
+            
             sepratorView.autoresizingMask = UIViewAutoresizingFlexibleWidth;//设置宽度屏幕跟随
             
             sepratorView.backgroundColor = marginColor;
@@ -350,9 +297,9 @@ static float const gap = 10;//取消按钮与上面的 gap
 
 - (void)show{
     
-    [self creatContainerView];
+   [self creatContainerView];
     
-    [self setUpButtons:self.contentView];
+   [self setUpButtons:self.contentView];
     
     UIWindow *window = [[[UIApplication sharedApplication] windows] lastObject];
     
@@ -414,23 +361,142 @@ static float const gap = 10;//取消按钮与上面的 gap
     
 }
 
-#pragma mark - rotate
-
-- (void)orientChange:(UIDeviceOrientation )orientation{
+#pragma mark - resizing
+/*
+ 系统会判断是否需要重新 layoutSubView
+ 例如:横屏-->竖屏
+ 例如:竖屏-->横屏
+ 横屏-->横屏:不会重新 layOut
+ 竖屏-->竖屏:不会重新 layOut
+ */
+-(void)layoutSubviews{
     
+    [super layoutSubviews];
+    
+    //由于是设置了 autosizng;但是还是要设置下尺寸
+    
+    //重新设置 frame
     float w = [UIScreen mainScreen].bounds.size.width;
     float h = [UIScreen mainScreen].bounds.size.height;
     self.frame = CGRectMake(0, 0, w, h);
     
-    float contentViewH = self.contentView.frame.size.height;
+    //布局 contentView
+    float contentViewH = [self calculateContentViewFrameWithRatate:YES].size.height;//h
     float contentViewW = w;
-    float contentViewY = h - contentViewH;
     float contentViewX = 0;
-    
+    float contentViewY = h - contentViewH;
     self.contentView.frame = CGRectMake(contentViewX, contentViewY, contentViewW, contentViewH);
     
-    [self setNeedsLayout];
+    //布局 titleLabel
+    if (![self isBlankString:self.title]) {
+        self.titleLabel.frame = CGRectMake( 0, normalMargin, self.contentView.frame.size.width, self.titleSize.height);
+    }
+    //布局 messageLabel
+    if (![self isBlankString:self.message]) {
+        
+        float messageY = [self isBlankString:self.title] ?
+        normalMargin :
+        normalMargin + self.titleSize.height + titleLabelBottomMargin;
+        self.messageLabel.frame = CGRectMake( 0, messageY, self.contentView.frame.size.width, self.messageSize.height);
+    }
+    //布局 gapView
+    if (![self isBlankString:self.title]||![self isBlankString:self.message]) {
+        
+        float delta = (self.type == CBWAlertSheetTypeCancelButton) ?
+        gap + buttonHeight:0;
+        float x = 0;
+        float y = self.contentView.frame.size.height - delta - margin - (self.items.count * buttonHeight + (self.items.count - 1) * margin);
+        self.marginView.frame = CGRectMake(x, y, w, margin);
+    }
     
+    //布局按钮
+    [self layoutButtons];
+}
+
+- (void)layoutButtons{
+
+    for (UIResponder *repsonse in self.contentView.subviews) {
+        
+        if ([repsonse isKindOfClass:[UIButton class]]) {
+            
+            UIButton *btn = (UIButton *)repsonse;
+            
+            [self resizingButton:btn];
+            
+        }else if ([repsonse isKindOfClass:[UIView class]]){
+            
+            UIView *view = (UIView *)repsonse;
+            
+            [self resizingMarginView:view];
+            
+        }
+    }
+}
+
+
+- (void)resizingButton:(UIButton *)btn{
+    
+     NSLog(@"button.tag=%ld",(long)btn.tag);
+    
+    if (btn.tag == cancelButtonTag) {
+        //处理取消按钮
+        float btnX = 0;
+        float btnY = self.contentView.frame.size.height - buttonHeight;
+        float btnW = screenW;
+        float btnH = buttonHeight;
+        [btn setFrame:CGRectMake(btnX,btnY, btnW, btnH)];
+        
+    }else{
+        //处理正常按钮
+        NSInteger i = btn.tag;
+
+        float delta = (self.type == CBWAlertSheetTypeCancelButton) ?
+        gap + buttonHeight:
+        0;
+        float base =self.contentView.bounds.size.height - (self.items.count * buttonHeight + (self.items.count - 1) * margin) - delta;
+        float btnX = 0;
+        float btnY = base +i * (buttonHeight + margin);
+        float btnW = screenW;//先预设值,后面屏幕旋转用UIViewAutoresizingFlexibleWidth
+        float btnH = buttonHeight;
+        [btn setFrame:CGRectMake(btnX,btnY, btnW, btnH)];
+        
+    }
+
+}
+
+- (void)resizingMarginView:(UIView *)view{
+    
+    if (view.tag == cancelGapViewtag) {
+        
+        view.frame = CGRectMake(0, self.cancelButtonFrame.origin.y - gap, self.contentView.frame.size.width, gap);
+     
+    }
+    
+    if (view.tag > sheetButtonMarginViewTag) {
+        
+        NSInteger i = view.tag - sheetButtonMarginViewTag;
+        
+        //设置分割线
+        if (i > 0)
+        {
+            
+            float delta = (self.type == CBWAlertSheetTypeCancelButton) ?
+            gap + buttonHeight:
+            0;
+            float base =self.contentView.bounds.size.height - (self.items.count * buttonHeight + (self.items.count - 1) * margin) - delta;
+            
+            float sepratorViewX = 0;
+            float sepratorViewY = base + (i - 1) * (buttonHeight + margin) + buttonHeight;
+            float sepratorViewW = screenW;//先预设值,后面屏幕旋转用UIViewAutoresizingFlexibleWidth
+            float sepratorViewH = margin;
+            CGRect sepratorFrame = CGRectMake(sepratorViewX, sepratorViewY, sepratorViewW, sepratorViewH);
+            view.frame = sepratorFrame;
+            
+        }
+        
+        
+    }
+
 }
 
 #pragma mark - private
@@ -466,12 +532,152 @@ static float const gap = 10;//取消按钮与上面的 gap
     
     return image;
 }
+
+/**
+ *   计算 contentView 的高度
+ */
+- (CGRect )calculateContentViewFrameWithRatate:(BOOL )rotated{
+    
+    CGFloat x = 0;
+    CGFloat y = rotated ? 0:[UIScreen mainScreen].bounds.size.height;
+    CGFloat w = [UIScreen mainScreen].bounds.size.width;
+    
+    NSString *title = self.title;
+    NSString *message = self.message;
+    
+    UIFont *font = [UIFont fontWithName:@"HelveticaNeue-Bold" size:titleFont];
+    CGSize titleSize = [title boundingRectWithSize:CGSizeMake( w - 2*lrMargin, 0) options:NSStringDrawingUsesLineFragmentOrigin attributes:@{NSFontAttributeName:font} context:nil].size;//用粗体计算,否者会出现横线,不知道为什么
+    UIFont *msgFont = [UIFont fontWithName:@"HelveticaNeue" size:messageFont];
+    CGSize messageSize = [message boundingRectWithSize:CGSizeMake( w - 2*lrMargin, 0) options:NSStringDrawingUsesLineFragmentOrigin attributes:@{NSFontAttributeName:msgFont} context:nil].size;
+    //h需要改变
+    float h = 0;
+    
+    if (![self isBlankString:title]||![self isBlankString:message]) {
+        h = h + 2 * normalMargin + margin;
+        
+    }
+    
+    if (![self isBlankString:title]) {
+        h = h + titleSize.height;
+    }
+    
+    if (![self isBlankString:message]) {
+        h = h + messageSize.height;
+    }
+    
+    if (![self isBlankString:message]&&![self isBlankString:title]) {
+        h = h + titleLabelBottomMargin;
+    }
+    
+    h = h + self.items.count * buttonHeight + (self.items.count - 1) * margin;
+    if (self.type == CBWAlertSheetTypeCancelButton) {h = h + gap + buttonHeight;}
+    
+    return  CGRectMake(x, y, w, h);
+}
 #pragma mark - setter && getter
 -(NSMutableArray *)items{
     if (_items == nil) {
         _items = [NSMutableArray array];
     }
     return _items;
+}
+
+-(CBWAlertSheetLabel *)titleLabel{
+    if (_titleLabel == nil) {
+        
+        CBWAlertSheetLabel *titleLabel = [[CBWAlertSheetLabel alloc]init];
+        titleLabel.textAlignment = NSTextAlignmentCenter;
+        titleLabel.text = self.title;
+        titleLabel.font = [UIFont fontWithName:@"HelveticaNeue" size:titleFont];
+        titleLabel.textColor = self.titleTextColor?self.titleTextColor:textDefaultColor;
+        titleLabel.textAlignment = NSTextAlignmentCenter;
+        titleLabel.backgroundColor = [UIColor whiteColor];
+        titleLabel.numberOfLines = 0;
+        titleLabel.autoresizingMask = UIViewAutoresizingFlexibleWidth;//设置宽度跟随屏幕
+        titleLabel.frame = CGRectMake( 0, normalMargin, self.contentView.frame.size.width, self.titleSize.height);
+
+        _titleLabel = titleLabel;
+    }
+    return _titleLabel;
+}
+
+-(CBWAlertSheetLabel *)messageLabel{
+    
+    if (_messageLabel == nil) {
+  
+        // 初始化label
+        CBWAlertSheetLabel *messageLabel = [[CBWAlertSheetLabel alloc]init];
+        messageLabel.textAlignment = NSTextAlignmentCenter;
+        messageLabel.textColor = self.messageTextColor?self.messageTextColor:textDefaultColor;
+        // label获取字符串
+        messageLabel.text = self.message;
+        messageLabel.backgroundColor = [UIColor whiteColor];
+        // label获取字体
+        messageLabel.font = [UIFont fontWithName:@"HelveticaNeue" size:messageFont];
+        // 设置无限换行
+        messageLabel.numberOfLines = 0;
+        float messageY = [self isBlankString:self.title] ?
+        normalMargin :
+        normalMargin + self.titleSize.height + titleLabelBottomMargin;
+        messageLabel.frame = CGRectMake( 0, messageY, self.contentView.frame.size.width, self.messageSize.height);
+        messageLabel.autoresizingMask = UIViewAutoresizingFlexibleWidth;//设置宽度跟随屏幕
+        
+        _messageLabel = messageLabel;
+          }
+    return _messageLabel;
+}
+
+-(UIView *)marginView{
+    
+    if (_marginView == nil) {
+        
+        float delta = (self.type == CBWAlertSheetTypeCancelButton) ?
+        gap + buttonHeight:0;
+        
+        float x = 0;
+        float y = self.contentView.frame.size.height - delta - margin - (self.items.count * buttonHeight + (self.items.count - 1) * margin);
+        float w = screenW;
+        float h = margin;
+        
+        UIView *margin = [[UIView alloc]initWithFrame:CGRectMake(x, y, w, h)];
+        margin.autoresizingMask = UIViewAutoresizingFlexibleWidth;
+        margin.backgroundColor = marginColor;
+        
+        _marginView = margin;
+    }
+    return _marginView;
+}
+-(CGSize)titleSize{
+    
+  
+    UIFont *font = [UIFont fontWithName:@"HelveticaNeue-Bold" size:titleFont];
+    CGSize titleSize = [self.title boundingRectWithSize:CGSizeMake( screenW - 2*lrMargin, 0) options:NSStringDrawingUsesLineFragmentOrigin attributes:@{NSFontAttributeName:font} context:nil].size;//用粗体计算,否者会出现横线,不知道为什么
+    _titleSize = titleSize;
+    
+    return _titleSize;
+}
+
+
+-(CGSize)messageSize{
+    
+    UIFont *msgFont = [UIFont fontWithName:@"HelveticaNeue" size:messageFont];
+    CGSize messageSize = [self.message boundingRectWithSize:CGSizeMake( screenW - 2*lrMargin, 0) options:NSStringDrawingUsesLineFragmentOrigin attributes:@{NSFontAttributeName:msgFont} context:nil].size;
+    
+    _messageSize = messageSize;
+    return _messageSize;
+}
+
+
+-(CGRect)cancelButtonFrame{
+    
+    float btnX = 0;
+    float btnY = self.contentView.bounds.size.height - buttonHeight;
+    float btnW = screenW;
+    float btnH = buttonHeight;
+    CGRect cancelButtonFrame = CGRectMake(btnX, btnY, btnW, btnH);
+    
+    _cancelButtonFrame = cancelButtonFrame;
+    return _cancelButtonFrame;
 }
 @end
 
